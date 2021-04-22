44b586cb75333d6f8d761fa3b0c79a167f8fc3d2
Guilhem <guilhem.dejeante@gmail.com>
2021-04-22 14:36:57 +0200

    commit

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios')
-var path = require('path');
 const handlebars = require('express-handlebars');
 
+var path = require('path');
+var ObjectId = require('mongodb').ObjectID;
+
 const app = express();
-
-
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/index.html'));
-// });
-
 
 // On définie handlebars comme moteur de notre app
 app.set('view engine', 'handlebars');
@@ -36,7 +32,7 @@ const dbName = 'tp-tensorflow';
 let db;
 
 MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-    console.log("Connexion au serveur MongoDB réussie");
+    // console.log("Connexion au serveur MongoDB réussie");
     db = client.db(dbName);
 });
 
@@ -44,15 +40,10 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (e
  * API
  */
 
-app.get('/airbnbs', async(req, res) => {
-    let nbSkipEntries = 0;
-
-    if (req.query.skip && parseInt(req.query.skip)) {
-        nbSkipEntries = parseInt(req.query.skip);
-    }
+app.get('/predictions', async(req, res) => {
 
     try {
-        const docs = await db.collection('predictions').find({}).skip(nbSkipEntries).limit(10).toArray();
+        const docs = await db.collection('predictions').find({}).limit(10).toArray();
         res.status(200).json(docs);
     } catch (err) {
         console.log(err);
@@ -60,45 +51,24 @@ app.get('/airbnbs', async(req, res) => {
     }
 });
 
-app.get('/airbnbs/:_id', async(req, res) => {
-    const _id = req.params._id;
+app.post('/create', async(req, res) => {
     try {
-        const docs = await db.collection('predictions').findOne({ _id });
+        const predictionData = req.body;
+        const prediction = await db.collection('predictions').insertOne(predictionData);
+        res.status(200).json(prediction);
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+});
+
+app.delete('/delete/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        console.log("id : " + id);
+        console.log("req param id : " + req.params.id);
+        const docs = await db.collection('predictions').deleteOne({ _id: ObjectId(id) });
         res.status(200).json(docs);
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
-});
-
-app.post('/airbnbs', async(req, res) => {
-    try {
-        const airbnbData = req.body;
-        const airbnb = await db.collection('predictions').insertOne(airbnbData);
-        res.status(200).json(airbnb);
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
-});
-
-app.put('/airbnbs/:_id', async(req, res) => {
-    try {
-        const _id = req.params._id;
-        const replacementAirbnb = req.body;
-        const airbnb = await db.collection('predictions').findOneAndUpdate({ _id }, { $set: replacementAirbnb });
-        res.status(200).json(airbnb);
-    } catch (err) {
-        console.log(err);
-        throw err;
-    }
-});
-
-app.delete('/airbnbs/:_id', async(req, res) => {
-    try {
-        const _id = req.params._id;
-        const airbnb = await db.collection('predictions').deleteOne({ _id });
-        res.status(200).json(airbnb);
     } catch (err) {
         console.log(err);
         throw err;
@@ -109,25 +79,14 @@ app.delete('/airbnbs/:_id', async(req, res) => {
  * FRONTEND
  */
 
-// Affiche tous les entrées airbnb
+// Affiche tous les entrées prediction
 app.get('/', async(req, res) => {
-    let nbSkipEntries = 0;
 
-    if (req.query.skip && parseInt(req.query.skip)) {
-        nbSkipEntries = parseInt(req.query.skip);
-    }
-
-    // Variables utiles pour la pagination
-    let nbSkipNext = nbSkipEntries + 10;
-    let nbSkipPrevious = (nbSkipEntries <= 10) ? 0 : nbSkipEntries - 10;
-
-    // Recupération des entrées airbnb
-    await axios.get('http://localhost:8080/airbnbs?skip=' + nbSkipEntries)
+    // Recupération des entrées prediction
+    await axios.get('http://localhost:8080/predictions')
         .then(resp => {
-            res.render('show-airbnbs', {
-                skip_next: nbSkipNext,
-                skip_previous: nbSkipPrevious,
-                airbnb_entries: resp.data
+            res.render('show-predictions', {
+                prediction_entries: resp.data
             });
         })
         .catch(err => {
@@ -136,26 +95,11 @@ app.get('/', async(req, res) => {
 });
 
 // Supprime une entrée
-app.get('/delete/:_id', async(req, res) => {
+app.get('/delete/:id', async(req, res) => {
     // Recupération d'une entrée
-    await axios.delete('http://localhost:8080/airbnbs/' + req.params._id)
+    await axios.delete('http://localhost:8080/delete/' + req.params.id)
         .then(() => {
-            console.log('deleted');
             res.redirect('back');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
-
-// Modifie une entrée
-app.get('/edit/:_id', async(req, res) => {
-    // Recupération d'une entrée
-    await axios.get('http://localhost:8080/airbnbs/' + req.params._id)
-        .then(resp => {
-            res.render('edit-airbnb', {
-                name: resp.data.name,
-            });
         })
         .catch(err => {
             console.log(err);
